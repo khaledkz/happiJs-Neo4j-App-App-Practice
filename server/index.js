a7878bdfceb6c663bb88469fefb2964ed6ec95fa
kharazzoro <kha.1991.14.2017@gmail.com>
2018-04-04 18:34:23 +0100

    handlebars template layout

--- server/index.js
+++ server/index.js
@@ -1,82 +1,78 @@
 'use strict';
 
+const Path = require('path');
 const Hapi = require('hapi');
-
-const Vision = require('vision');
-const Handlebars = require('handlebars');
-
+const Hoek = require('hoek');
 
 const server = Hapi.server({
     port: 3000,
     host: 'localhost'
 });
 
-// server.route({
-//     method: 'GET',
-//     path: '/',
-//     handler: (request, h) => {
 
-//         return ('<h1>Hello, world!</h1>');
-//     }
-// });
-
-// server.route({
-//     method: 'GET',
-//     path: '/{name}',
-//     handler: (request, h) => {
-
-//         return 'Hello, ' + encodeURIComponent(request.params.name) + '!';
-//     }
-// });
-
-// const init = async () => {
-
-//     await server.start();
-//     console.log(`Server running at: ${server.info.uri}`);
-// };
 
 
 const init = async () => {
-
+    await server.register(require('vision'));
     await server.register(require('inert'));
-    await server.register(Vision);
 
+   
     server.views({
-        engines: { html: Handlebars },
+        engines: {
+            html: require('handlebars')
+        },
         relativeTo: __dirname,
-        path: 'view/handlebars/'
+        layout:true,
+        path: './view/templates',
+        layoutPath: './view/templates/layout',
+     });
+   
+     server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+    
+            return 'Hello, world!';
+        }
     });
+    
+    
+    server.route({
+        method: 'GET',
+        path: '/page',
+        // handler: {view:{
+        //     template: 'main'
+        // }}
+        handler:(req,h)=>{
+             return h.view('main')
+        }
+    });
+
+ 
 
     server.route({
         method: 'GET',
-        path: '/',
+        path: '/file',
         handler: (request, h) => {
     
             return h.file('./public/pages/hello.html');
         }
     });
-
+ 
+    
     server.route({
         method: 'GET',
-        path: '/main',
+        path: '/{name}',
         handler: (request, h) => {
-            return h.view('main',{msg:'Handlebars Tamplet'})
+    
+            return 'Hello, ' + encodeURIComponent(request.params.name) + '!';
         }
-    })
-
-    server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler: (request, h) => {
-
-        return h.file('./public/images/hapi.jpg');
-    }
-});
+    });
 
     await server.start();
+
     console.log(`Server running at: ${server.info.uri}`);
 };
-
 
 process.on('unhandledRejection', (err) => {
 
