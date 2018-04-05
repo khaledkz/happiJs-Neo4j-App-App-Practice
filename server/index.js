a17483dc274633759b68179c088074d334f41b4c
kharazzoro <kha.1991.14.2017@gmail.com>
2018-04-05 11:40:00 +0100

    static file serve all file by sigle route

--- server/index.js
+++ server/index.js
@@ -4,13 +4,18 @@ const Path = require('path');
 const Hapi = require('hapi');
 const Hoek = require('hoek');
 
+ 
+
+
 const server = Hapi.server({
     port: 3000,
-    host: 'localhost'
+    host: 'localhost',
+    routes: {
+        files: {
+            relativeTo: Path.join(__dirname, './public')
+        }
+    }
 });
-
-
-
 
 const init = async () => {
     await server.register(require('vision'));
@@ -26,36 +31,99 @@ const init = async () => {
         path: './view/templates',
         layoutPath: './view/templates/layout',
      });
-   
+
+
      server.route({
         method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-    
-            return 'Hello, world!';
+        path: '/{p*}',
+        handler: {
+            directory: {
+                path: Path.join(__dirname, 'public'),
+                listing: false,
+                index: false
+            }
         }
     });
-    
-    
+
+
+     server.route({
+        method: 'GET',
+        path: '/stylesheet/style.css',
+        handler: {
+            file: {
+                path: 'stylesheet/style.css',
+                filename: 'style.css', // override the filename in the Content-Disposition header
+                mode: 'attachment', // specify the Content-Disposition is an attachment
+                lookupCompressed: true // allow looking for script.js.gz if the request allows it
+            }
+        }
+    });
+
+    // server.route({
+    //     method: 'GET',
+    //     path: '/images/hapi.jpg',
+    //     handler: {
+    //         file: {
+    //             path: 'images/hapi.jpg',
+    //             filename: 'hapi.jpg', // override the filename in the Content-Disposition header
+    //             mode: 'attachment', // specify the Content-Disposition is an attachment
+    //          }
+    //     }
+    // });
+
+
+    // server.route({
+    //     method: 'GET',
+    //     path: '/{param*}',
+    //     handler: {
+    //         directory: {
+    //             path: 'public/images/{param*}',
+    //             listing: true
+    //         }
+    //     }
+    // });
+        
     server.route({
         method: 'GET',
-        path: '/page',
+        path: '/',
         // handler: {view:{
         //     template: 'main'
         // }}
         handler:(req,h)=>{
-             return h.view('main')
+             return h.view('index')
         }
     });
 
- 
+    server.route({
+        method: 'GET',
+        path: '/service',
+        handler:(req,h)=>{
+             return h.view('service')
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/contact',
+        handler:(req,h)=>{
+             return h.view('contact')
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/branches',
+        handler:(req,h)=>{
+             return h.view('branches')
+        }
+    });
 
     server.route({
         method: 'GET',
         path: '/file',
         handler: (request, h) => {
     
-            return h.file('./public/pages/hello.html');
+            return h.file('./pages/hello.html');
         }
     });
  
